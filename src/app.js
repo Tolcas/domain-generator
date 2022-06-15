4d20643d554634d69cb3381d4a22773bcd460c53
Tolcas <zavala.juanep@gmail.com>
2022-06-15 00:06:11 +0000

    tarea terminada

--- src/app.js
+++ src/app.js
@@ -5,7 +5,21 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let extension = [".com", ".es", ".cl", ".ar"];
+let newDomain = [];
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < extension.length; l++) {
+          newDomain.push("www." + pronoun[i] + adj[j] + noun[k] + extension[l]);
+        }
+      }
+    }
+  }
+  document.querySelector("p").innerHTML = newDomain;
 };
